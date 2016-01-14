4a1abe437f44578ed3d97a518a83919e9406ce10
yirong.zd <yirong.zd@alibaba-inc.com>
2016-01-14 17:16:10 +0800

    修改进度条

--- src/js/view/pieces/segs/elements/demo-progressbar.js
+++ src/js/view/pieces/segs/elements/demo-progressbar.js
@@ -1,7 +1,7 @@
 define(function() {
     function Demo() {
         //选中指定的dom元素进行初始化
-        $('.progressbar-sample').progressbar({
+        $('.progressbar-sample:first').progressbar({
             //给进度条最外层指定样式
             css: 'progressbar-test-style',
             //需要设置的初始化的进度的百分比
@@ -9,6 +9,15 @@ define(function() {
             //可选default 条形和 circle环形
             shape: 'circle'
         });
+		
+        //选中指定的dom元素进行初始化
+        $('.progressbar-sample1').progressbar({
+            //给进度条最外层指定样式
+            css: 'progressbar-test-style',
+            //需要设置的初始化的进度的百分比
+            progress: '40%'
+            //可选default 条形和 circle环形
+        });		
     }
     return Demo;
 });
