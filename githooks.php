802ebe913c3d853f0c1f6c5bd3a82aaec8a27cd1
冯伟 <vigo.feng@ndpmedia.com>
2016-01-19 13:19:46 +0000

    Update githooks.php

--- githooks.php
+++ githooks.php
@@ -1,6 +1,10 @@
 <?php
     error_reporting ( E_ALL );
-    exec("cd /home/uikit/");
-    $return= exec("git --git-dir=/home/uikit/.git pull");
-    echo $return;   
+    echo "Start deployment";
+    echo exec("git reset --hard origin/master");
+    echo exec("git clean -f");
+    echo exec("git --git-dir=/home/uikit/.git pull");
+    echo "Changing permissions...";
+    echo exec("chown -R www *");
+    echo "Finished";
 ?>
