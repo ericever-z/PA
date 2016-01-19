a20b6273c3211e6eae3bf7bfd2e58e7f42f58974
vigo feng <vigo.feng@ndpmedia.com>
2016-01-19 11:58:36 +0800

    update githooks.php

--- githooks.php
+++ githooks.php
@@ -1,8 +1,6 @@
 <?php
-    error_reporting ( E_ALL );
-    $dir = '/home/uikit/';//该目录为git检出目录
-    $password = '12354';//这是是密码
+    error_reporting ( E_ALL )
     exec("cd /home/uikit/");
-    $return= exec("git pull ");
+    $return= exec("git --git-dir=/home/uikit/.git pull");
     echo $return;   
 ?>
