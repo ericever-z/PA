2c17319b8684144bc8c03da00816f1135a0ca0a7
vigo.feng <ilfw@qq.com>
2016-01-26 17:51:32 +0800

    update daterangepicker

---
+++ src/js/view/pieces/segs/elements/demo-daterangepicker.js
@@ -0,0 +1,19 @@
+define(['moment'], function(moment) {
+    function Demo() {
+        $('.daterangepicker-sample').daterangepicker({
+            "ranges": {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            "startDate": "01/20/2016",
+            "endDate": "01/26/2016"
+        }, function(start, end, label) {
+            console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+        });
+    }
+    return Demo;
+});
