82f503d57e196124862b707859bac2e4e0c53a69
冯文肇 <ilfw@qq.com>
2016-03-08 15:32:41 +0800

    fix

--- src/js/view/pieces/segs/elements/demo-daterangepicker.js
+++ src/js/view/pieces/segs/elements/demo-daterangepicker.js
@@ -1,39 +1,5 @@
 define(['moment'], function(moment) {
     function Demo() {
-
-
-        var tpl = '<div class="daterangepicker dropdown-menu">' +
-            '<div class="clearfix">'       + 
-            '<div class="calendar left">' +
-            '<div class="daterangepicker_input">' +
-            '<input class="input-mini" type="text" name="daterangepicker_start" value="" />' +
-            '<i class="fa fa-calendar glyphicon glyphicon-calendar"></i>' +
-            '<div class="calendar-time">' +
-            '<div></div>' +
-            '<i class="fa fa-clock-o glyphicon glyphicon-time"></i>' +
-            '</div>' +
-            '</div>' +
-            '<div class="calendar-table"></div>' +
-            '</div>' +
-            '<div class="calendar right">' +
-            '<div class="daterangepicker_input">' +
-            '<input class="input-mini" type="text" name="daterangepicker_end" value="" />' +
-            '<i class="fa fa-calendar glyphicon glyphicon-calendar"></i>' +
-            '<div class="calendar-time">' +
-            '<div></div>' +
-            '<i class="fa fa-clock-o glyphicon glyphicon-time"></i>' +
-            '</div>' +
-            '</div>' +
-            '<div class="calendar-table"></div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="ranges">' +
-            '<div class="range_inputs">' +
-            '<button class="applyBtn" disabled="disabled" type="button"></button> ' +
-            '<button class="cancelBtn" type="button"></button>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
         var range = {
             'Today': [moment(), moment()],
             'Last 3 Days': [moment().subtract(2, 'days'), moment()],
@@ -41,24 +7,51 @@ define(['moment'], function(moment) {
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         };
 
+        //普通用法
         $('.daterangepicker-sample').daterangepicker({
             "ranges": range,
             "startDate": "01/20/2016",
             "endDate": "01/26/2016"
-        }, function(start, end, label) {
-            console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
-        });
+        }, function(start, end, label) {});
 
+        //时间段
         $('.daterangepicker-sample1').daterangepicker({
             "ranges": range,
             "startDate": "01/20/2016",
             "endDate": "01/26/2016",
-           // "template": tpl,
-            "opens": "nxdc",
-            "alwaysShowCalendars": true  
-        }, function(start, end, label) {
-            console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
-        });
+            "opens": "nxdc-mulitple",
+            "alwaysShowCalendars": true,
+            locale: {
+                "applyLabel": "OK",
+                "cancelLabel": '',
+            },
+            autoApply: true
+        }, function(start, end, label) {});
+
+        //单个日期
+        $('.daterangepicker-sample2').daterangepicker({
+                singleDatePicker: true,
+                showDropdowns: true
+            },
+            function(start, end, label) {
+
+            });
+
+        //单个日期,含时分秒
+        $('.daterangepicker-sample3').daterangepicker({
+                "singleDatePicker": true,
+                "showDropdowns": true,
+                "opens": "nxdc-single",
+                "timePicker": true,
+                "timePicker24Hour": true,
+                "locale": {
+                    "format": 'MM/DD/YYYY h:mm'
+                }
+            },
+            function(start, end, label) {
+
+            });
+
     }
     return Demo;
 });
