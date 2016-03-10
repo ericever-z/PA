c6b11c95284b128b632a3428dc3c4f0dade8f0c5
冯伟 <vigo.feng@ndpmedia.com>
2016-03-10 16:35:55 +0800

    修改githooks.php

--- githooks.php
+++ githooks.php
@@ -1,29 +1,11 @@
 <?php
     error_reporting ( E_ALL );
-    echo "Show status";
-    echo "<br>";
     echo exec("git status");
-    echo "<br>";
-    echo "Start deployment";
-    echo "<br>";
     echo exec("git reset --hard");
-    echo "<br>";
-    echo exec("git clean -f");
-    echo "<br>";
+    echo exec("git clean -f -d");
     echo exec("git pull");
-    echo "<br>";
-    echo exec("git pull");
-    echo "<br>";
-    echo exec("git pull");
-    echo "<br>";
-    echo exec("git pull");
-    echo "<br>";
-    echo exec("git pull");
-    echo "<br>";
-    echo "Changing permissions...";
-    echo "<br>";
     echo exec("chown -R www *");
     echo exec("chown -R www:www .git");
-    echo "<br>";
-    echo "Finished";
+    $arr = array ('msg'=>'request received!');
+    echo json_encode($arr);
 ?>
