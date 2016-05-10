ebba12b0c8d7c08d125e0ebc2369c64ff9c85fb9
yirong.zd <yirong.zd@alibaba-inc.com>
2016-05-10 15:52:08 +0800

    更新uikit 1.0.38

--- src/js/view/pieces/segs/elements/demo-alert.js
+++ src/js/view/pieces/segs/elements/demo-alert.js
@@ -17,7 +17,19 @@ define(function(){
 							console.log("hello 这里是回调显示");
 						}
 					});
-				});			
+				});	
+        
+				$("button.btn-info").click(function(){
+					showAlert({
+						icon:"<i class='glyphicon glyphicon-globe'></i>",
+						content:"这里显示的是一个地球，用来测试图片在右边，文字在左边问题！dddddddd！dsfdfbdshjfdfdhfdsfhdjfdshfdsfhjdfdsjfhdsfjdhfdjfkdhsfjdfhdsjfdshfjdsfhdsjfhdsjfdhsfjdshfdsjfdhsfjdshfjdsfhdsjfdsfhdjfdhdsjfhdsjkfdhskfdhsghdgdsjdshjhdsjhdsjgkdlsdsldhdsfdskfhdsfjdsfjdfhdjfhdfjdfhdsjfvdsjddfdsfdsfdfdsfdsfhdsfdsfdbdb db",
+                        backdrop:false,//不显示背景色
+						btnOK:"确定",
+						callback:function(dom){//关闭alert窗口，回调函数
+							console.log("hello 这里是回调显示");
+						}
+					});
+				});	        
 	};
 	
 	return Demo;
