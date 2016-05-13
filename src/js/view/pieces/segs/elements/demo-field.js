c6165b593ca7c28ab6f1c682fcde6d58cb4f809b
yirong.zd <yirong.zd@alibaba-inc.com>
2016-05-13 15:43:05 +0800

    更新组件库

--- src/js/view/pieces/segs/elements/demo-field.js
+++ src/js/view/pieces/segs/elements/demo-field.js
@@ -7,7 +7,15 @@ define(function(){
         });
         $("#intxt").click(function(){
             hwd.getContent();
-        }); 	
+        }); 
+        /****
+        ** 回填数据
+        ****/
+         $("#fill").click(function(){
+             //回填的数据，字符串
+           var str = "sdfdsfs{{1130|hello world}} sfsdfdsfds{{1130|hello world}} sdfsdf#b#ndsfdsf{{1130|hello world}}123234234hello"
+            hwd.fill(str);
+        });        
 	}
 	return Demo;
 });
